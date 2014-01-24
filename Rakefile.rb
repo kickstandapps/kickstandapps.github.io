1a94e6b9bb35788174a6feb2c855f0620bc5a07a
Kickstand Apps <developer@kickstandapps.com>
2014-01-23 21:24:01 -0600

    Update JQuery

--- Rakefile.rb
+++ Rakefile.rb
@@ -16,7 +16,7 @@ task :pingomatic do
   begin
     require 'xmlrpc/client'
     puts '* Pinging ping-o-matic'
-    XMLRPC::Client.new('rpc.pingomatic.com', '/').call('weblogUpdates.extendedPing', 'mademistakes.com' , 'http://mademistakes.com', 'http://mademistakes.com/atom.xml')
+    XMLRPC::Client.new('rpc.pingomatic.com', '/').call('weblogUpdates.extendedPing', 'kickstandapps.com' , 'http://kickstandapps.com', 'http://kickstandapps.com/atom.xml')
   rescue LoadError
     puts '! Could not ping ping-o-matic, because XMLRPC::Client could not be found.'
   end
@@ -29,7 +29,7 @@ task :sitemapgoogle do
     require 'net/http'
     require 'uri'
     puts '* Pinging Google about our sitemap'
-    Net::HTTP.get('www.google.com', '/webmasters/tools/ping?sitemap=' + URI.escape('http://mademistakes.com/sitemap.xml'))
+    Net::HTTP.get('www.google.com', '/webmasters/tools/ping?sitemap=' + URI.escape('http://kickstandapps.com/sitemap.xml'))
   rescue LoadError
     puts '! Could not ping Google about our sitemap, because Net::HTTP or URI could not be found.'
   end
@@ -42,7 +42,7 @@ task :sitemapbing do
     require 'net/http'
     require 'uri'
     puts '* Pinging Bing about our sitemap'
-    Net::HTTP.get('www.bing.com', '/webmaster/ping.aspx?siteMap=' + URI.escape('http://mademistakes.com/sitemap.xml'))
+    Net::HTTP.get('www.bing.com', '/webmaster/ping.aspx?siteMap=' + URI.escape('http://kickstandapps.com/sitemap.xml'))
   rescue LoadError
     puts '! Could not ping Bing about our sitemap, because Net::HTTP or URI could not be found.'
   end
--- assets/js/_main.js
+++ assets/js/_main.js
@@ -48,6 +48,7 @@ $(document).ready(function() {
     }
   });
 
+
 // Initialise FlexSlider for Carousels
 $(window).load(function() {
   $('.flexslider').flexslider({
