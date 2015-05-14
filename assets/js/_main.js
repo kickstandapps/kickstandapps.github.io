03ea55f152d1e3509c441c7f658c196059a8f1c4
Kickstand Apps <developer@kickstandapps.com>
2015-05-14 14:57:48 -0500

    Updated link checker.

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -64,7 +64,8 @@ $(window).load(function() {
 // Form validation
 document.getElementById("form1").onsubmit = function () {
     if (document.getElementById("Validate").value) {
-        if(new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(status_text) == false) {
+        var string = document.getElementById("TextBox").value;
+        if(new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(string) == false) {
             window.location.href = "?success";
             return false;
         }
